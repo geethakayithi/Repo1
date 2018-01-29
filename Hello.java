a353bc2adff165b0965c8327fc84c31364d33a5a
geethakayithi <geethakayithi@gmail.com>
2018-01-29 13:24:51 -0500

    modified Hello.java

--- Hello.java
+++ Hello.java
@@ -8,7 +8,7 @@ public static void main(String[] args)
 for (int i=0; i<=10 ; i++)
 {
 
-System.out.println("Hi I am Geetha Kayithi");
+System.out.println("Hi I am Geetha Kayithi and i am learning Java Script");
 
 }
 
